59679ef21786ceeff8e39c7ef2ecfd7f1d817fd5
Grecu Narcis <narcisgrecu07@gmail.com>
2024-03-07 20:02:38 +0200

    refactored users form using useRef hook

--- src/shared/components/button/Button.tsx
+++ src/shared/components/button/Button.tsx
@@ -1,9 +1,15 @@
-import "./Button.css";
+import './Button.css';
 
 export function Button(props: any) {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.buttonMessage}
-    </button>
-  );
+    return (
+        <button
+            type={props.type}
+            className={
+                'button' + ' ' + (props.className ? props.className : '')
+            }
+            onClick={props.onClick}
+        >
+            {props.buttonMessage}
+        </button>
+    );
 }
