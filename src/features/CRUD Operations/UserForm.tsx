1f2680e3234c94b304860a893bf7d708b98142a9
Grecu Narcis <narcisgrecu07@gmail.com>
2024-03-08 10:17:32 +0200

    made user form more general for use

--- src/features/CRUD Operations/UserForm.tsx
+++ src/features/CRUD Operations/UserForm.tsx
@@ -1,64 +1,15 @@
+import { UserFormType } from '../../pages/Add User Page/AddUserPage';
 import { FormEntry } from './FormEntry';
-import { User } from '../../models/user';
-import { Button } from '../../shared/components/button/Button';
-import { useUsersContext } from '../../contexts/UsersContext';
 
-import { useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
 import './UserForm.css';
 
-function handleOnClick(
-    idInput: React.RefObject<HTMLInputElement>,
-    firstNameInput: React.RefObject<HTMLInputElement>,
-    lastNameInput: React.RefObject<HTMLInputElement>,
-    urlInput: React.RefObject<HTMLInputElement>,
-): User {
-    if (!idInput.current || !firstNameInput.current || !lastNameInput.current || !urlInput.current)
-        throw new Error('Inputs references are null');
-
-    if (
-        !idInput.current.value ||
-        !firstNameInput.current.value ||
-        !lastNameInput.current.value ||
-        !urlInput.current.value
-    )
-        throw new Error('You must provide values for each field!');
-
-    const userId: number = parseInt(idInput.current.value),
-        userFirstName: string = firstNameInput.current.value,
-        userLastName: string = lastNameInput.current.value,
-        userUrl: string = urlInput.current.value;
-
-    return new User(userId, userFirstName, userLastName, userUrl);
-}
-
-export function UserForm() {
-    const idInput = useRef<HTMLInputElement>(null);
-    const firstNameInput = useRef<HTMLInputElement>(null);
-    const lastNameInput = useRef<HTMLInputElement>(null);
-    const urlInput = useRef<HTMLInputElement>(null);
-
-    const navigate = useNavigate();
-
-    const usersContext = useUsersContext();
-
+export function UserForm(props: UserFormType) {
     const formEntries = [
-        { label: 'ID', ref: idInput },
-        { label: 'First Name', ref: firstNameInput },
-        { label: 'Last Name', ref: lastNameInput },
-        { label: 'URL', ref: urlInput },
+        { label: 'ID', ref: props.idInput },
+        { label: 'First Name', ref: props.firstNameInput },
+        { label: 'Last Name', ref: props.lastNameInput },
+        { label: 'URL', ref: props.urlInput },
     ];
-
-    const handleOnClickWrapper = () => {
-        try {
-            const inputUser = handleOnClick(idInput, firstNameInput, lastNameInput, urlInput);
-            usersContext.addUser(inputUser);
-
-            navigate('/');
-        } catch (error) {
-            alert(error);
-        }
-    };
 
     return (
         <div className='form-div'>
@@ -67,8 +18,6 @@ export function UserForm() {
                     <FormEntry key={entry.label} label={entry.label} ref={entry.ref} />
                 ))}
             </form>
-
-            <Button type='submit' buttonMessage='Add user' className='form-button' onClick={handleOnClickWrapper} />
         </div>
     );
 }
--- src/pages/Add User Page/AddUserPage.tsx
+++ src/pages/Add User Page/AddUserPage.tsx
@@ -1,16 +1,81 @@
 import { UserForm } from '../../features/CRUD Operations/UserForm';
 import { Layout } from '../../shared/components/layout/Layout';
+import { Button } from '../../shared/components/button/Button';
+import { User } from '../../models/user';
+
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { useUsersContext } from '../../contexts/UsersContext';
 
 import './AddUserPage.css';
 
+export type UserFormType = {
+    idInput: React.RefObject<HTMLInputElement>;
+    firstNameInput: React.RefObject<HTMLInputElement>;
+    lastNameInput: React.RefObject<HTMLInputElement>;
+    urlInput: React.RefObject<HTMLInputElement>;
+};
+
+function handleOnClick(
+    idInput: React.RefObject<HTMLInputElement>,
+    firstNameInput: React.RefObject<HTMLInputElement>,
+    lastNameInput: React.RefObject<HTMLInputElement>,
+    urlInput: React.RefObject<HTMLInputElement>,
+): User {
+    if (!idInput.current || !firstNameInput.current || !lastNameInput.current || !urlInput.current)
+        throw new Error('Inputs references are null');
+
+    if (
+        !idInput.current.value ||
+        !firstNameInput.current.value ||
+        !lastNameInput.current.value ||
+        !urlInput.current.value
+    )
+        throw new Error('You must provide values for each field!');
+
+    const userId: number = parseInt(idInput.current.value),
+        userFirstName: string = firstNameInput.current.value,
+        userLastName: string = lastNameInput.current.value,
+        userUrl: string = urlInput.current.value;
+
+    return new User(userId, userFirstName, userLastName, userUrl);
+}
+
 export function AddUserPage() {
     document.title = 'Add user';
+
+    const idInput = useRef<HTMLInputElement>(null);
+    const firstNameInput = useRef<HTMLInputElement>(null);
+    const lastNameInput = useRef<HTMLInputElement>(null);
+    const urlInput = useRef<HTMLInputElement>(null);
+
+    const navigate = useNavigate();
+    const usersContext = useUsersContext();
+
+    const handleOnClickWrapper = () => {
+        try {
+            const inputUser = handleOnClick(idInput, firstNameInput, lastNameInput, urlInput);
+            usersContext.addUser(inputUser);
+            navigate('/');
+        } catch (error) {
+            alert(error);
+        }
+    };
 
     return (
         <Layout>
             <div className='main-page-container'>
                 <div className='main-title'>Add user</div>
-                <UserForm />
+
+                <UserForm
+                    idInput={idInput}
+                    firstNameInput={firstNameInput}
+                    lastNameInput={lastNameInput}
+                    urlInput={urlInput}
+                />
+
+                <Button type='submit' buttonMessage='Add user' className='form-button' onClick={handleOnClickWrapper} />
             </div>
         </Layout>
     );
